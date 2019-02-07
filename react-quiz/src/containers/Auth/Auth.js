25e22add904a771f5a1f777b98ab0663fc5869c3
Eugene Kofman <kofman@doctornet.ru>
2019-02-07 11:53:58 +0300

    make form authorization

--- react-quiz/src/containers/Auth/Auth.js
+++ react-quiz/src/containers/Auth/Auth.js
@@ -1,13 +1,49 @@
 import React, {Component} from 'react';
+import classes from './Auth.module.css'
+import Button from '../../components/UI/Button/Button'
+import Input from '../../components/UI/Input/Input'
 
 class Auth extends Component {
-    render() {
-        return (
-            <div>
-                <h1>Auth</h1>
-            </div>
-        );
-    }
+
+  loginHandler = () => {
+
+  }
+
+  registerHandler = () => {
+
+  }
+
+	submitHandler = event => {
+  	event.preventDefault()
+	}
+
+  render() {
+    return (
+      <div className={classes.Auth}>
+        <div>
+          <h1>Авторизация</h1>
+
+          <form onSubmit={this.submitHandler} className={classes.AuthForm}>
+            <Input label="Email"/>
+
+            <Input label="Пароль" errorMessage="test"/>
+            <Button
+              type="success"
+              onClick={this.loginHandler}
+            >
+              Войти
+            </Button>
+            <Button
+              type="primary"
+              onClick={this.registerHandler}
+            >
+              Зарегистрироваться
+            </Button>
+          </form>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Auth;
