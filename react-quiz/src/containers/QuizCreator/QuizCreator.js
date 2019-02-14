826ac98a484779a9d4667756b1b0406a36a91de2
Eugene Kofman <kofman@doctornet.ru>
2019-02-14 19:02:59 +0300

    n

--- react-quiz/src/containers/QuizCreator/QuizCreator.js
+++ react-quiz/src/containers/QuizCreator/QuizCreator.js
@@ -1,7 +1,7 @@
 import React, {Component, Fragment} from 'react';
 import classes from './QuizCreator.module.css'
 import Button from '../../components/UI/Button/Button'
-import {createControl} from '../../form/formFramework'
+import {createControl, validate, validateForm, } from '../../form/formFramework'
 import Input from '../../components/UI/Input/Input'
 import Select from '../../components/UI/Select/Select'
 
@@ -30,6 +30,7 @@ class QuizCreator extends Component {
 
   state = {
     quiz: [],
+    isFormValid: false,
     rightAnswerID: 1,
     formControls: createFormControls()
   }
@@ -38,8 +39,8 @@ class QuizCreator extends Component {
       event.preventDefault()
   }
 
-  addQuestionHandler = () => {
-
+  addQuestionHandler = event => {
+    event.preventDefault()
   }
 
   createQuizHandler = () => {
@@ -47,7 +48,19 @@ class QuizCreator extends Component {
   }
 
   changeHandler = (value, controlName) => {
+    const formControls = {...this.state.formControls}
+    const control = {...formControls[controlName]}
 
+    control.touched = true
+    control.value = value
+    control.valid = validate(control.value, control.validation)
+
+    formControls[controlName] = control
+
+    this.setState({
+      formControls,
+      isFormValid: validateForm(formControls)
+    })
   }
 
   renderControls() {
@@ -63,7 +76,7 @@ class QuizCreator extends Component {
             shouldValidate = {!!control.validation}
             touched = {control.touched}
             errorMessage = {control.errorMessage}
-            onChange = {event => this.changeHandler(event.target.value(), controlName)}
+            onChange = {event => this.changeHandler(event.target.value, controlName)}
           />
           {index === 0 ? <hr/> : null}
         </Fragment>
@@ -73,7 +86,7 @@ class QuizCreator extends Component {
 
   selectChangeHandler = (event) => {
     this.setState({
-      rightAnswerID: event.target.value
+      rightAnswerID: +event.target.value
     })
   }
 
@@ -104,12 +117,14 @@ class QuizCreator extends Component {
             <Button
               type="primary"
               onClick={this.addQuestionHandler}
+              disabled={!this.state.isFormValid}
             >
               Добавить вопрос
             </Button>
             <Button
               type="success"
               onClick={this.createQuizHandler}
+              disabled={this.state.quiz.length === 0}
             >
               Создать тест
             </Button>
--- react-quiz/src/form/formFramework.js
+++ react-quiz/src/form/formFramework.js
@@ -1,3 +1,9 @@
+/**
+ *
+ * @param config
+ * @param validation
+ * @returns {{validation: *, valid: boolean, touched: boolean, value: string}}
+ */
 export function createControl(config, validation) {
   return {
     ...config,
@@ -6,5 +12,36 @@ export function createControl(config, validation) {
     touched: false,
     value: ''
   }
+}
+
+/**
+ *
+ * @param value
+ * @param validation
+ * @returns {*}
+ * @constructor
+ */
+export function validate(value, validation = null) {
+  if (!validation) return true
+
+  let isValid = true
+
+  if (validation.required) {
+    isValid = value.trim() !== '' && isValid
+  }
+
+  return isValid
+}
+
+export function validateForm(formControls) {
+  let isFormValid = true
+
+  for (let control in formControls) {
+    if (formControls.hasOwnProperty(control)) {
+      isFormValid = formControls[control].valid && isFormValid
+    }
+  }
+
+  return isFormValid
 }
 
